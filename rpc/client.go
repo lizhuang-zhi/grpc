b889b605ec13149fd325fb2eecbdb08a4c6a2761
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2023-11-13 21:38:18 +0800

    基于TCP协议的RPC

--- rpc/client.go
+++ rpc/client.go
@@ -11,22 +11,14 @@ type ClientArgs struct {
 }
 
 func main() {
-	// 建立HTTP连接
-	client, err := rpc.DialHTTP("tcp", "127.0.0.1:9091")
+	// 建立TCP连接
+	client, err := rpc.Dial("tcp", "127.0.0.1:9091")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
 
-	args := &ClientArgs{10, 20}
-
-	// 异步调用
-	var reply2 int
-	divCall := client.Go("ServiceA.Add", args, &reply2, nil)
-	replyCall := <-divCall.Done // 接收调用结果
-	fmt.Println(replyCall.Error)
-	fmt.Println(reply2)
-
 	// 同步调用
+	args := &ClientArgs{10, 20}
 	var reply int
 	err = client.Call("ServiceA.Add", args, &reply)
 	if err != nil {
@@ -34,4 +26,10 @@ func main() {
 	}
 	fmt.Printf("ServiceA.Add: %d+%d=%d\n", args.X, args.Y, reply)
 
+	// 异步调用
+	var reply2 int
+	divCall := client.Go("ServiceA.Add", args, &reply2, nil)
+	replyCall := <-divCall.Done // 接收调用结果
+	fmt.Println(replyCall.Error)
+	fmt.Println(reply2)
 }
--- rpc/service.go
+++ rpc/service.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"net/rpc"
 )
 
@@ -11,8 +11,10 @@ type Args struct {
 	X, Y int
 }
 
+// ServiceA 自定义一个结构体类型
 type ServiceA struct{}
 
+// Add 为ServiceA类型增加一个可导出的Add方法
 func (s *ServiceA) Add(args *Args, reply *int) error {
 	*reply = args.X + args.Y
 	return nil
@@ -20,12 +22,14 @@ func (s *ServiceA) Add(args *Args, reply *int) error {
 
 func main() {
 	service := new(ServiceA)
-	rpc.Register(service)  // 注册rpc服务
-	rpc.HandleHTTP()  // 基于HTTP协议
+	rpc.Register(service) // 注册RPC服务
 	l, e := net.Listen("tcp", ":9091")
 	if e != nil {
-		log.Fatal("listen error: ", e)
+		log.Fatal("listen error:", e)
 	}
-
-	http.Serve(l, nil)
+	for {
+		fmt.Println("Connect.....")  // 启动时会建立一次连接, 然后client请求会再次执行
+		conn, _ := l.Accept()
+		rpc.ServeConn(conn)
+	}
 }
